0b00ea7c58a455640d3233aab7b44f0af6e0cbed
Jeremiah England <englandtuning@gmail.com>
2022-11-24 13:47:40 -0500

    chore: Add tests/__init__.py with a docstring

---
+++ tests/__init__.py
@@ -0,0 +1,5 @@
+"""Utilities for the shapely stubs tests.
+
+Ideas may be stolen from the pandas stubs repository as we go.
+See: https://github.com/pandas-dev/pandas-stubs/blob/main/tests/__init__.py
+"""
