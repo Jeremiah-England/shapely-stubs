777cfbe28c0b54c470d40f9343c9c108eff451e3
Jeremiah England <englandtuning@gmail.com>
2022-12-01 07:45:30 -0500

    draft: Saving work on transform

--- shapely-stubs/coordinates.pyi
+++ shapely-stubs/coordinates.pyi
@@ -1,4 +1,40 @@
-def transform(geometry, transformation, include_z: bool = ...): ...
+from collections.abc import Callable, Sequence
+from typing import TypeAlias, TypeVar, overload
+
+import numpy as np
+import numpy.typing as npt
+from shapely.lib import Geometry
+
+# A reasonable placeholder till I figure out the exact type of this thing!
+_GeometryArrayLike: TypeAlias = Sequence[Geometry]
+_GeometryTypeVar = TypeVar("_GeometryTypeVar", bound=Geometry)
+_TransformFunction: TypeAlias = Callable[
+    [npt.NDArray[np.float64]], npt.NDArray[np.float64]
+]
+
+@overload
+def transform(
+    geometry: _GeometryTypeVar,
+    transformation: _TransformFunction,
+    include_z: bool = ...,
+) -> _GeometryTypeVar: ...
+@overload
+def transform(
+    geometry: _GeometryArrayLike,
+    transformation: _TransformFunction,
+    include_z: bool = ...,
+) -> _GeometryArrayLike: ...
+@overload
+def transform(
+    geometry: Geometry | _GeometryArrayLike,
+    transformation: _TransformFunction,
+    include_z: bool = ...,
+) -> Geometry | _GeometryArrayLike: ...
+def transform(
+    geometry: Geometry | _GeometryArrayLike,
+    transformation: _TransformFunction,
+    include_z: bool = ...,
+) -> Geometry | _GeometryArrayLike: ...
 def count_coordinates(geometry): ...
 def get_coordinates(geometry, include_z: bool = ..., return_index: bool = ...): ...
 def set_coordinates(geometry, coordinates): ...
